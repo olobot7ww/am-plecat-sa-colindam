7bac8280f36ac50b20ac5c2418aa4db6af978af3
olobot7ww <robocodebotanica10@gmail.com>
2021-04-24 14:41:53 +0300

    My first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const addButton = document.querySelector("#addButton");
+const todoList = document.querySelector(".toDoList");
+const input = document.querySelector("#input");
+let notes = [];
+
+
+
+
+function addNotice() {
+  notes.push(input.value);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  input.value = "";
+  location.reload();
+  return false;
+}
+
+function getNotes() {
+  if (JSON.parse(localStorage.getItem("notes")) !== null) {
+    notes = JSON.parse(localStorage.getItem("notes"));
+    for (let i = 0; i < notes.length; i++) {
+      noteCreator(notes[i], i);
+    }
+  }
+}
+
+function noteCreator(e, i) {
+  const note = document.createElement("div");
+  const contentNote = document.createElement("div");
+  const del = document.createElement("div");
+  note.classList.add("note");
+  contentNote.classList.add("contentNote");
+  del.classList.add("del");
+
+  // del.addEventListener("click", () => {
+  //   console.log("clicked", i);
+  // });
+
+  const newContentNote = document.createTextNode(e);
+  const delContentNote = document.createTextNode("x");
+  contentNote.appendChild(newContentNote);
+  del.appendChild(delContentNote);
+  note.appendChild(contentNote);
+  note.appendChild(del);
+  todoList.appendChild(note);
+}
+
+getNotes();
+addButton.addEventListener("click", addNotice);
